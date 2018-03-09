1e67f01dc815c7dea30af2250490948b82ed5ea3
abra2abra <abra2abra@mailinator.com>
2018-03-09 19:24:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -19,4 +19,5 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign({}, obj, {[key]: value})
 }
 
-console.log(updateObjectWithKeyAndValue())
+console.log(updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup'));
+console.log(recipe);
